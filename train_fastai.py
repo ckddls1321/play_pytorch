2c2552ace898c7c52938804ee56bdc32a93bcec7
ChangIn.Choi <ckddls1321@users.noreply.github.com>
2020-11-15 23:05:05 +0900

    Update train_fastai.py

--- train_fastai.py
+++ train_fastai.py
@@ -44,3 +44,62 @@ learn = faster_rcnn.fastai.learner(
     dls=[train_dl, valid_dl], model=model, metrics=metrics
 )
 learn.fine_tune(10, lr=1e-4)
+
+
+
+
+
+# COCO Example 
+# pip install icevision[all] icedata
+
+from icevision.all import *
+
+url = "https://cvbp.blob.core.windows.net/public/datasets/object_detection/odFridgeObjects.zip"
+dest_dir = "fridge"
+
+# Loading Data
+data_dir = icedata.load_data(url, dest_dir)
+
+# Parser
+class_map = ClassMap(["milk_bottle", "carton", "can", "water_bottle"])
+parser = parsers.voc(annotations_dir=data_dir / "odFridgeObjects/annotations/",
+                    images_dir=data_dir / "odFridgeObjects/images",
+                    class_map=class_map)
+
+# Records
+train_records, valid_records = parser.parse()
+
+# Transforms
+train_tfms = tfms.A.Adapter([*tfms.A.aug_tfms(size=384, presize=512), tfms.A.Normalize()])
+valid_tfms = tfms.A.Adapter([*tfms.A.resize_and_pad(384), tfms.A.Normalize()])
+
+# Datasets
+train_ds = Dataset(train_records, train_tfms)
+valid_ds = Dataset(valid_records, valid_tfms)
+
+# DataLoaders
+train_dl = efficientdet.train_dl(train_ds, batch_size=16, num_workers=4, shuffle=True)
+valid_dl = efficientdet.valid_dl(valid_ds, batch_size=16, num_workers=4, shuffle=False)
+
+# Model and Metrics
+model = efficientdet.model(model_name="tf_efficientdet_lite0", num_classes=len(class_map), img_size=size)
+metrics = [COCOMetric(metric_type=COCOMetricType.bbox)]
+
+# Training using Fastai
+learn = efficientdet.fastai.learner(dls=[train_dl, valid_dl], model=model, metrics=metrics)
+learn.fine_tune(50, 1e-2, freeze_epochs=20)
+
+# Inference
+# DataLoader
+infer_dl = efficientdet.infer_dl(valid_ds, batch_size=8)
+# Predict
+samples, preds = efficientdet.predict_dl(model, infer_dl)
+# Show samples
+imgs = [sample["img"] for sample in samples]
+show_preds(
+    imgs=imgs[:6],
+    preds=preds[:6],
+    class_map=class_map,
+    denormalize_fn=denormalize_imagenet,
+    ncols=3,
+)
